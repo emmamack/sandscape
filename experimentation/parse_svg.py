2f8a0603761ce892c91995e10f4578dcba721494
Emma Mack <emack@olin.edu>
2025-02-21 15:13:51 -0500

    svg meat detector detects some but not all meat

--- experimentation/parse_svg.py
+++ experimentation/parse_svg.py
@@ -34,6 +34,8 @@ curve_types_to_expected_length = {
 def split_raw_curves(raw):
     split_at_letter = re.findall(r'[a-zA-Z][^a-zA-Z]*', raw)
     curves = []
+
+    # TODO: check and discard 'z'? ('end curve')
     for curve_block in split_at_letter:
         marker = curve_block[0]
         body = [float(x) for x in re.findall(r'-?\d+\.?\d*', curve_block[1:])]
@@ -53,9 +55,10 @@ def discretize_bezier(raw, prev_pt):
             [prev_pt.x, prev_pt.x+node_set[0], prev_pt.x+node_set[2], prev_pt.x+node_set[4]],
             [prev_pt.y, prev_pt.y+node_set[1], prev_pt.y+node_set[3], prev_pt.y+node_set[5]],
         ])
-        print(nodes)
+        # print(nodes)
         bezier_curve_obj = bezier.Curve(nodes, degree=3)
 
+        # TODO: normalize by line length
         s_vals = np.linspace(0.0, 1.0, 10)
         evaluator_output = bezier_curve_obj.evaluate_multi(s_vals)
         for x, y in zip(evaluator_output[0], evaluator_output[1]):
@@ -89,26 +92,60 @@ def parse_multiple_curves(curves_raw):
     return pts
         
 
-tree = et.parse("..\svg_examples\Archimedean_spiral.svg")
-root = tree.getroot()
+def get_layer_above_meat(layer):
 
-if root.attrib['version'] != '1.1':
-    print(f"Warning! Unsupported svg version: {root['version']}")
+    print(f"current layer.attrib: {layer.attrib}")
+    if layer.attrib == {}:
+        return None
 
-#TODO: handle variable numbers of layers before meat appears
+    for child in layer:
+        print(f"evaluating child: {child.attrib}")
+        if child.attrib.get('d', None):
+            print("it's this one!")
+            return layer
 
-all_pts = []
-for child in root:
-    curves_raw = child.attrib['d']
-    all_pts.extend(parse_multiple_curves(curves_raw))
+    for child in layer:
+        layer_below_evaluation = get_layer_above_meat(child)
+        if layer_below_evaluation:
+            return layer_below_evaluation
     
-pts_decoded = [pt.to_tuple() for pt in all_pts]
-x_coords = [pt[0] for pt in pts_decoded]
-y_coords = [pt[1] for pt in pts_decoded]
-plt.figure()
-plt.plot(x_coords, y_coords, 'b-')  # 'b-' means blue line
-plt.scatter(x_coords, y_coords, c='red', s=50)  # Add points as red dots
-plt.grid(True)
-plt.axis('equal')  # Make the scale equal on both axes
-plt.title('SVG Path Visualization')
-plt.show()
+    raise RuntimeError("oops, shouldn't have gotten here")
+
+def get_pts_from_file(file):
+    tree = et.parse(file)
+    root = tree.getroot()
+    print(f"root: {root.attrib}")
+    if root.attrib['version'] != '1.1':
+        print(f"Warning! Unsupported svg version: {root.attrib['version']}")
+
+    layer_above_meat = get_layer_above_meat(root)
+    print(layer_above_meat.attrib)
+
+    all_pts = []
+    for child in layer_above_meat:
+        print(child.attrib)
+        curves_raw = child.attrib['d']
+
+        all_pts.extend(parse_multiple_curves(curves_raw))
+    
+    return all_pts
+
+def plot_pts(pts): 
+    pts_decoded = [pt.to_tuple() for pt in pts]
+    x_coords = [pt[0] for pt in pts_decoded]
+    y_coords = [pt[1] for pt in pts_decoded]
+    plt.figure()
+    plt.plot(x_coords, y_coords, 'b-')  # 'b-' means blue line
+    plt.scatter(x_coords, y_coords, c='red', s=50)  # Add points as red dots
+    plt.grid(True)
+    plt.axis('equal')  # Make the scale equal on both axes
+    plt.title('SVG Path Visualization')
+    plt.show()
+
+if __name__ == "__main__":
+    # svg_file = "..\svg_examples\cabin.svg"
+    # svg_file = "..\svg_examples\Archimedean_spiral.svg"
+    svg_file = "..\svg_examples\eye-drops-svgrepo-com.svg"
+
+    pts = get_pts_from_file(svg_file)
+    plot_pts(pts)
