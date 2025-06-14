edefa027ff43a2868a6d07734da50831471ec044
Emma Mack <emack@olin.edu>
2025-06-14 15:55:29 -0400

    color blocks respond to touch

--- arduino_sketches/control_lights/control_lights.ino
+++ arduino_sketches/control_lights/control_lights.ino
@@ -1,74 +1,113 @@
-// A basic everyday NeoPixel strip test program.
-
-// NEOPIXEL BEST PRACTICES for most reliable operation:
-// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
-// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
-// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
-// - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
-//   connect GROUND (-) first, then +, then data.
-// - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
-//   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
-// (Skipping these may work OK on your workbench but can fail in the field)
-
 #include <Adafruit_NeoPixel.h>
+#include <Adafruit_MCP23X17.h>
+
 #ifdef __AVR__
  #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
 #endif
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1:
-#define LED_PIN    6
-
-// How many NeoPixels are attached to the Arduino?
 #define LED_COUNT 284
+#define LED_PIN 6
 
-// Declare our NeoPixel strip object:
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGBW + NEO_KHZ400);
-// Argument 1 = Number of pixels in NeoPixel strip
-// Argument 2 = Arduino pin number (most are valid)
-// Argument 3 = Pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 
+// ~~~~~~~~~~ Sensor stuff ~~~~~~~~~~~~~~~~~~~~~ //
 
-// setup() function -- runs once at startup --------------------------------
+#define SENSOR_PIN_0 15
+#define SENSOR_PIN_1 14
+#define SENSOR_PIN_2 13
+#define SENSOR_PIN_3 12
+#define SENSOR_PIN_4 11
+#define SENSOR_PIN_5 10
+#define SENSOR_PIN_6 9
+#define SENSOR_PIN_7 8
+#define SENSOR_PIN_8 7
+#define SENSOR_PIN_9 6
+#define SENSOR_PIN_10 5
+#define SENSOR_PIN_11 4
+#define SENSOR_PIN_12 3
+#define SENSOR_PIN_13 2
+#define SENSOR_PIN_14 1
+#define SENSOR_PIN_15 0
+
+int res0 = 0;
+int res1 = 1;
+int res2 = 2;
+int res3 = 3;
+int res4 = 4;
+int res5 = 5;
+int res6 = 6;
+int res7 = 7;
+int res8 = 8;
+int res9 = 9;
+int res10 = 10;
+int res11 = 11;
+int res12 = 12;
+int res13 = 13;
+int res14 = 14;
+int res15 = 15;
+
+Adafruit_MCP23X17 mcp;
+
 
 void setup() {
-  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
-  // Any other board, you can remove this part (but no harm leaving it):
-#if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-  clock_prescale_set(clock_div_1);
-#endif
-  // END of Trinket-specific code.
-
+  Serial.begin(9600);
+  
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
   strip.setBrightness(60); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+  if (!mcp.begin_I2C()) {
+    Serial.println("I2C error.");
+    while (1);
+  }
+
+  mcp.pinMode(SENSOR_PIN_0, INPUT);
+  mcp.pinMode(SENSOR_PIN_1, INPUT);
+  mcp.pinMode(SENSOR_PIN_2, INPUT);
+  mcp.pinMode(SENSOR_PIN_3, INPUT);
+  mcp.pinMode(SENSOR_PIN_4, INPUT);
+  mcp.pinMode(SENSOR_PIN_5, INPUT);
+  mcp.pinMode(SENSOR_PIN_6, INPUT);
+  mcp.pinMode(SENSOR_PIN_7, INPUT);
+  mcp.pinMode(SENSOR_PIN_8, INPUT);
+  mcp.pinMode(SENSOR_PIN_9, INPUT);
+  mcp.pinMode(SENSOR_PIN_10, INPUT);
+  mcp.pinMode(SENSOR_PIN_11, INPUT);
+  mcp.pinMode(SENSOR_PIN_12, INPUT);
+  mcp.pinMode(SENSOR_PIN_13, INPUT);
+  mcp.pinMode(SENSOR_PIN_14, INPUT);
+  mcp.pinMode(SENSOR_PIN_15, INPUT);
 }
 
-
-// loop() function -- runs repeatedly as long as board is on ---------------
-
-void loop() {
-  // Fill along the length of the strip in various colors...
-//  colorWipe(strip.Color(255,   0,   0), 50); // Red
-//  colorWipe(strip.Color(  0, 255,   0), 50); // Green
-//  colorWipe(strip.Color(  0,   0, 255), 50); // Blue
-//
-//  // Do a theater marquee effect in various colors...
-//  theaterChase(strip.Color(127, 127, 127), 50); // White, half brightness
-//  theaterChase(strip.Color(127,   0,   0), 50); // Red, half brightness
-//  theaterChase(strip.Color(  0,   0, 127), 50); // Blue, half brightness
-
-  rainbow(20);             // Flowing rainbow cycle along the whole strip
-//  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
+void readTouchSensors() {
+  res0 = mcp.digitalRead(SENSOR_PIN_0);
+  res1 = mcp.digitalRead(SENSOR_PIN_1);
+  res2 = mcp.digitalRead(SENSOR_PIN_2);
+  res3 = mcp.digitalRead(SENSOR_PIN_3);
+  res4 = mcp.digitalRead(SENSOR_PIN_4);
+  res5 = mcp.digitalRead(SENSOR_PIN_5);
+  res6 = mcp.digitalRead(SENSOR_PIN_6);
+  res7 = mcp.digitalRead(SENSOR_PIN_7);
+  res8 = mcp.digitalRead(SENSOR_PIN_8);
+  res9 = mcp.digitalRead(SENSOR_PIN_9);
+  res10 = mcp.digitalRead(SENSOR_PIN_10);
+  res11 = mcp.digitalRead(SENSOR_PIN_11);
+  res12 = mcp.digitalRead(SENSOR_PIN_12);
+  res13 = mcp.digitalRead(SENSOR_PIN_13);
+  res14 = mcp.digitalRead(SENSOR_PIN_14);
+  res15 = mcp.digitalRead(SENSOR_PIN_15);
+  
+  Serial.print("["); 
+  Serial.print(res0); Serial.print(","); Serial.print(res1); Serial.print(","); 
+  Serial.print(res2); Serial.print(","); Serial.print(res3); Serial.print(","); 
+  Serial.print(res4); Serial.print(","); Serial.print(res5); Serial.print(","); 
+  Serial.print(res6); Serial.print(","); Serial.print(res7); Serial.print(","); 
+  Serial.print(res8); Serial.print(","); Serial.print(res9); Serial.print(","); 
+  Serial.print(res10); Serial.print(","); Serial.print(res11); Serial.print(","); 
+  Serial.print(res12); Serial.print(","); Serial.print(res13); Serial.print(","); 
+  Serial.print(res14); Serial.print(","); Serial.print(res15); Serial.print(","); 
+  Serial.println("]");
 }
-
-
-// Some functions of our own for creating animated effects -----------------
 
 // Fill strip pixels one after another with a color. Strip is NOT cleared
 // first; anything there will be covered pixel by pixel. Pass in color
@@ -79,6 +118,7 @@ void colorWipe(uint32_t color, int wait) {
   for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
     strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
     strip.show();                          //  Update strip to match
+    readTouchSensors();
     delay(wait);                           //  Pause for a moment
   }
 }
@@ -140,4 +180,41 @@ void theaterChaseRainbow(int wait) {
       firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
     }
   }
+}
+
+void  touchResponseBlock(uint32_t base_color, int wait) {
+  readTouchSensors();
+  int touchSensorReading[16] = {res0, res1, res2, res3, res4, res5, res6, res7, res8, res9, res10, res11, res12, res13, res14, res15};
+
+  int curr_pixel = 0;
+  for(int s=0; s<16; s++) {
+    uint32_t color;
+    if(touchSensorReading[s]==0) {
+      color = strip.Color(127, 127, 127); // white
+    } else {
+      color = base_color;
+    }
+    
+    for(int i=curr_pixel; i<curr_pixel+18; i++) {
+      strip.setPixelColor(i, color);
+    }
+
+    curr_pixel += 18;
+  }
+  
+  strip.show();
+  delay(wait);
+}
+
+void loop() {
+  touchResponseBlock(strip.Color( 0, 0, 255), 10);
+
+//  colorWipe(strip.Color(255,   0,   0), 50); // Red
+//  colorWipe(strip.Color(  0, 255,   0), 50); // Green
+//  colorWipe(strip.Color(  0,   0, 255), 50); // Blue
+
+//  theaterChase(strip.Color(127, 127, 127), 50); // White, half brightness
+
+//  rainbow(20);             // Flowing rainbow cycle along the whole strip
+//  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
 }
