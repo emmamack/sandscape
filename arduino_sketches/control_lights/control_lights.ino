329ee6ab104d122eb11bda4e43ce88c1feeffdfb
Emma Mack <emack@olin.edu>
2025-06-14 17:50:46 -0400

    remove unused code

--- arduino_sketches/control_lights/control_lights.ino
+++ arduino_sketches/control_lights/control_lights.ino
@@ -1,9 +1,9 @@
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_MCP23X17.h>
 
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
+//#ifdef __AVR__
+// #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+//#endif
 
 #define LED_COUNT 284
 #define LED_PIN 6
@@ -47,10 +47,7 @@ int res14 = 14;
 int res15 = 15;
 
 int sectionSizes[18] = {17, 18, 18, 18, 17, 18, 18, 18, 17, 18, 18, 18, 17, 18, 18, 18, 17, 18}; // two too long because of the looparound check
-//int beforeTrapezoidProfile[18] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 28, 56, 84, 112, 140, 168, 196, 224, 252};
 int beforeTrapezoidProfile[18] = {14, 28, 42, 56, 70, 84, 98, 112, 126, 140, 154, 168, 182, 196, 210, 224, 238, 252};
-
-//int afterTrapezoidProfile[18] = {252, 224, 196, 168, 140, 112, 84, 56, 28, 0, 0, 0, 0, 0, 0, 0, 0};
 int afterTrapezoidProfile[18] = {252, 238, 224, 210, 196, 182, 168, 154, 140, 126, 112, 98, 84, 70, 56, 42, 28, 14};
 
 Adafruit_MCP23X17 mcp;
@@ -59,9 +56,9 @@ Adafruit_MCP23X17 mcp;
 void setup() {
   Serial.begin(9600);
   
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(60); // Set BRIGHTNESS (max = 255)
+  strip.begin();
+  strip.show();
+  strip.setBrightness(60);
 
   if (!mcp.begin_I2C()) {
     Serial.println("I2C error.");
@@ -116,79 +113,6 @@ void readTouchSensors() {
   Serial.println("]");
 }
 
-// Fill strip pixels one after another with a color. Strip is NOT cleared
-// first; anything there will be covered pixel by pixel. Pass in color
-// (as a single 'packed' 32-bit value, which you can get by calling
-// strip.Color(red, green, blue) as shown in the loop() function above),
-// and a delay time (in milliseconds) between pixels.
-void colorWipe(uint32_t color, int wait) {
-  for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
-    strip.setPixelColor(i, color);         //  Set pixel's color (in RAM)
-    strip.show();                          //  Update strip to match
-    readTouchSensors();
-    delay(wait);                           //  Pause for a moment
-  }
-}
-
-// Theater-marquee-style chasing lights. Pass in a color (32-bit value,
-// a la strip.Color(r,g,b) as mentioned above), and a delay time (in ms)
-// between frames.
-void theaterChase(uint32_t color, int wait) {
-  for(int a=0; a<10; a++) {  // Repeat 10 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
-      // 'c' counts up from 'b' to end of strip in steps of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
-      }
-      strip.show(); // Update strip with new contents
-      delay(wait);  // Pause for a moment
-    }
-  }
-}
-
-// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
-void rainbow(int wait) {
-  // Hue of first pixel runs 5 complete loops through the color wheel.
-  // Color wheel has a range of 65536 but it's OK if we roll over, so
-  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
-  // means we'll make 5*65536/256 = 1280 passes through this loop:
-  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
-    // strip.rainbow() can take a single argument (first pixel hue) or
-    // optionally a few extras: number of rainbow repetitions (default 1),
-    // saturation and value (brightness) (both 0-255, similar to the
-    // ColorHSV() function, default 255), and a true/false flag for whether
-    // to apply gamma correction to provide 'truer' colors (default true).
-    strip.rainbow(firstPixelHue);
-    // Above line is equivalent to:
-    // strip.rainbow(firstPixelHue, 1, 255, 255, true);
-    strip.show(); // Update strip with new contents
-    delay(wait);  // Pause for a moment
-  }
-}
-
-// Rainbow-enhanced theater marquee. Pass delay time (in ms) between frames.
-void theaterChaseRainbow(int wait) {
-  int firstPixelHue = 0;     // First pixel starts at red (hue 0)
-  for(int a=0; a<30; a++) {  // Repeat 30 times...
-    for(int b=0; b<3; b++) { //  'b' counts from 0 to 2...
-      strip.clear();         //   Set all pixels in RAM to 0 (off)
-      // 'c' counts up from 'b' to end of strip in increments of 3...
-      for(int c=b; c<strip.numPixels(); c += 3) {
-        // hue of pixel 'c' is offset by an amount to make one full
-        // revolution of the color wheel (range 65536) along the length
-        // of the strip (strip.numPixels() steps):
-        int      hue   = firstPixelHue + c * 65536L / strip.numPixels();
-        uint32_t color = strip.gamma32(strip.ColorHSV(hue)); // hue -> RGB
-        strip.setPixelColor(c, color); // Set pixel 'c' to value 'color'
-      }
-      strip.show();                // Update strip with new contents
-      delay(wait);                 // Pause for a moment
-      firstPixelHue += 65536 / 90; // One cycle of color wheel over 90 frames
-    }
-  }
-}
-
 void  touchResponseBlock(int wait) {
   int base_color = strip.Color( 0, 0, 255, 0); // blue
   readTouchSensors();
@@ -196,28 +120,11 @@ void  touchResponseBlock(int wait) {
 
   int curr_pixel = 0;
   for(int s=1; s<17; s++) {
-//    if(touchSensorReading[s]==1) {
-//       strip.fill(strip.Color(0, 0, 255, 255), curr_pixel, sectionSizes[s]);
-
-//      int white_amount = 0;
-//      for(int i=curr_pixel; i<curr_pixel+9; i++) {
-//        strip.setPixelColor(i, strip.Color(0, 0, 255, white_amount));
-//        white_amount += 28;
-//      }
-//      for(int i=curr_pixel+9; i<curr_pixel+18; i++) {
-//        strip.setPixelColor(i, strip.Color(0, 0, 255, white_amount));
-//        white_amount -= 28;
-//      }
-//    } else {
-//      strip.fill(base_color, curr_pixel, sectionSizes[s]);
-//    }
-
     for(int i=0; i<18; i++) {
       int white_amount = (touchSensorReading[s+1] * beforeTrapezoidProfile[i]) + 
                          (touchSensorReading[s] * 255) +
                          (touchSensorReading[s-1] * afterTrapezoidProfile[i]);
       if (white_amount > 255) white_amount = 255;
-//      Serial.println(white_amount);
       strip.setPixelColor(curr_pixel+i, strip.Color(0, 0, 255, white_amount));
     }
 
@@ -230,13 +137,4 @@ void  touchResponseBlock(int wait) {
 
 void loop() {
   touchResponseBlock(10);
-
-//  colorWipe(strip.Color(255,   0,   0), 50); // Red
-//  colorWipe(strip.Color(  0, 255,   0), 50); // Green
-//  colorWipe(strip.Color(  0,   0, 255), 50); // Blue
-
-//  theaterChase(strip.Color(127, 127, 127), 50); // White, half brightness
-
-//  rainbow(20);             // Flowing rainbow cycle along the whole strip
-//  theaterChaseRainbow(50); // Rainbow-enhanced theaterChase variant
 }
